0cfae11664ba81f45c62f04061ca6678efcdd3b4
Brianca Knight <briancaknight@gmail.com>
2023-10-30 13:42:58 -0700

    Finished event listeners and present location message.
    Co-authored-by: Monica Barboza<MonBoza@gmail.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -30,16 +30,37 @@ function Places(location, landmarks, season) {
 
 Places.prototype.locationMessage = function() {
   return "I have been to " + this.location + ", there we saw the " + this.landmarks + " during " + this.season;
-}
+};
 
 //UI Logic
 let worldTravel = new WorldTravel();
 
-function handelFormSubmission(event) {
-  event.preventDefault();
-
+function listPlace(placesToDisplay) {
+  let placesDiv = document.querySelector("div#places");
+  placesDiv.innerText = " ";
+  const ul = document.createElement("ul");
+  Object.keys(placesToDisplay.places).forEach(function(key){
+    const place = placesToDisplay.findPlace(key);
+    const li = document.createElement("li");
+    li.append(place.locationMessage());
+    li.setAttribute("id", place.id);
+    ul.append(li);
+  });
+  placesDiv.append(ul);
 }
 
-window.addEventListener("load", function (){
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const inputtedLocation = document.querySelector("#newLocation").value; 
+  const inputtedLandmarks = document.querySelector("#newLandmarks").value; 
+  const inputtedSeason = document.querySelector("#newSeason").value; 
+  let newPlace = new Places(inputtedLocation, inputtedLandmarks, inputtedSeason);
+  worldTravel.addPlace(newPlace);
+  listPlace(worldTravel);
+  console.log(worldTravel.places);
+}
 
+
+window.addEventListener("load", function (){
+  document.querySelector("form#new-place").addEventListener("submit", handleFormSubmission);
 });
